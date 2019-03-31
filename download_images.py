975d1fc04ee82a560a92b28a4eea3ad7b8e9e083
fg91 <fabiograetz@googlemail.com>
2019-03-31 17:22:15 +0200

    Add functionality to resize downloaded image to a max size
    If a max size is passed as argument and the smaller side of the image
    exceeds this max size, the image is rescaled so that the smaller side
    is equal to max size while mainting the aspect ratio.

--- download_images.py
+++ download_images.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from PIL import Image
+from PIL.Image import LANCZOS
 import requests
 from pathlib import Path
 import argparse
@@ -9,21 +10,34 @@ parser = argparse.ArgumentParser()
 parser.add_argument("datafile", help='tsv file containing image captions and urls', type=str)
 parser.add_argument("startIdx", type=int)
 parser.add_argument("endIdx", type=int)
+parser.add_argument("maxSize", type=int, nargs='?', default=None)
 args = parser.parse_args()
 
 captions_and_links = pd.read_csv(args.datafile, sep="\t",header=None)
+
+def calc_new_size(img, max_sz):
+    width, height = img.size
+    smaller_side, resize_ratio = (width, max_sz/width) if width < height else (height, max_sz/height)
+    if smaller_side <= max_sz:
+        return None
+    else:
+        return (int(width * resize_ratio), int(height * resize_ratio))
 
 def get_image_w_caption(df, PATH, idx):
     try:
         caption, link = df.iloc(0)[idx]
         img = Image.open(requests.get(link, stream=True, timeout=10).raw)
+        if args.maxSize is not None:
+            new_size = calc_new_size(img, args.maxSize)
+            if new_size is not None:
+                img = img.resize(new_size, resample=LANCZOS)
         img.save(PATH/(str(idx)+".png"), format='png')
     except:
         return None
     return str(str(idx)+".png"), caption
 
 PATH = Path('data/')
-SUB_PATH = PATH/'downloaded'
+SUB_PATH = PATH/'downloadedPics'
 PATH.mkdir(exist_ok=True)
 SUB_PATH.mkdir(exist_ok=True)
 
