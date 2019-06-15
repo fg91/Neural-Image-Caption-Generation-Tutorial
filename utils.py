30c9bc0bcc1c00a446001e8beb5a19d4bd8accb3
fg91 <fabiograetz@googlemail.com>
2019-06-16 00:43:11 +0200

    Adapt size of individual pictures

--- utils.py
+++ utils.py
@@ -20,24 +20,23 @@ def show_img(im, figsize=None, ax=None, alpha=1, cmap=None):
     ax.get_yaxis().set_visible(False)
     return ax
 
-def visualize_attention(im, pred, alphas, denorm, vocab, att_size=14, sz=224):
+def visualize_attention(im, pred, alphas, denorm, vocab, att_size=14, sz=224):                                                       
     cap_len = len(pred)
     alphas = alphas.view(-1,1,  att_size, att_size).cpu().data.numpy()
     alphas = np.maximum(0, alphas)
     alphas -= alphas.min()
     alphas /= alphas.max()
-    
-    #figure, axes = plt.subplots(cap_len//3 + 1,3, figsize=(10,8))
-    figure, axes = plt.subplots(4,3, figsize=(10,8))
-
+ 
+    figure, axes = plt.subplots(cap_len//5 + 1,5, figsize=(12,8))
+ 
     for i, ax in enumerate(axes.flat):
-        if i < cap_len:
+        if i <= cap_len:
             ax = show_img(denorm(im), ax=ax)
             if i > 0:
                 mask = np.array(Image.fromarray(alphas[i - 1,0]).resize((sz,sz)))
-                blurred_mask = gaussian_filter(mask, sigma=10)
+                blurred_mask = gaussian_filter(mask, sigma=8)
                 show_img(blurred_mask, ax=ax, alpha=0.5, cmap='afmhot')
                 draw_text(ax, (0,0), vocab.itos[pred[i - 1]])
-            else:
-                ax.axis('off')
+        else:
+            ax.axis('off')
     plt.tight_layout()
